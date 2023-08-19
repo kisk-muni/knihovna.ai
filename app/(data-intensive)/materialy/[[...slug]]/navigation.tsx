a2210da34abed9783cff22b342c03a9b5078e884
cernockyd <24832873+cernockyd@users.noreply.github.com>
2023-08-19 11:00:22 +0200

    fix svg props

--- app/(data-intensive)/materialy/[[...slug]]/navigation.tsx
+++ app/(data-intensive)/materialy/[[...slug]]/navigation.tsx
@@ -19,7 +19,7 @@ const topNavIcons: { [key: string]: any } = {
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      stroke-width="2"
+      strokeWidth="2"
       stroke="#D68A67"
       style={{
         stroke: "color(display-p3 0.8392 0.5412 0.4039)",
@@ -28,8 +28,8 @@ const topNavIcons: { [key: string]: any } = {
       className="w-6 h-6 -mt-0.5 inline-block mr-2"
     >
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         d="M3.75 12h16.5m-16.5 3.75h16.5M3.75 19.5h16.5M5.625 4.5h12.75a1.875 1.875 0 010 3.75H5.625a1.875 1.875 0 010-3.75z"
       />
     </svg>
@@ -42,9 +42,9 @@ const topNavIcons: { [key: string]: any } = {
       className="w-6 h-6 -mt-0.5 inline-block mr-2"
     >
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
         stroke="#D68A67"
         style={{
           stroke: "color(display-p3 0.8392 0.5412 0.4039)",
@@ -59,7 +59,7 @@ const topNavIcons: { [key: string]: any } = {
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      stroke-width="2"
+      strokeWidth="2"
       stroke="#D68A67"
       style={{
         stroke: "color(display-p3 0.8392 0.5412 0.4039)",
@@ -68,13 +68,13 @@ const topNavIcons: { [key: string]: any } = {
       className="w-6 h-6 -mt-0.5 inline-block mr-2"
     >
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         d="M21.75 6.75a4.5 4.5 0 01-4.884 4.484c-1.076-.091-2.264.071-2.95.904l-7.152 8.684a2.548 2.548 0 11-3.586-3.586l8.684-7.152c.833-.686.995-1.874.904-2.95a4.5 4.5 0 016.336-4.486l-3.276 3.276a3.004 3.004 0 002.25 2.25l3.276-3.276c.256.565.398 1.192.398 1.852z"
       />
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         d="M4.867 19.125h.008v.008h-.008v-.008z"
       />
     </svg>
