6b09e5eed71a6e0e649a109b4bd6abe975848c72
cernockyd <24832873+cernockyd@users.noreply.github.com>
2023-08-19 11:00:47 +0200

    create website screenshotting route

---
+++ app/screenshot/route.ts
@@ -0,0 +1,41 @@
+import chromium from "chrome-aws-lambda";
+import playwright from "playwright-core";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest, res: NextResponse) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const hasURL = searchParams.has("url");
+    const url = hasURL ? searchParams.get("url") : null;
+    if (!url) throw new Error("URL to screenshot not set.");
+    const browser = await playwright.chromium.launch({
+      args: chromium.args,
+      headless: true,
+    });
+    const page = await browser.newPage({
+      viewport: {
+        width: 1200,
+        height: 630,
+      },
+    });
+    await page.goto(url, {
+      timeout: 15 * 1000,
+    });
+    const data = await page.screenshot({
+      type: "png",
+    });
+    await browser.close();
+    return new Response(data, {
+      status: 200,
+      headers: {
+        "Cache-Control": "s-maxage=31536000, stale-while-revalidate",
+        "Content-Type": "image/png",
+      },
+    });
+  } catch (e: any) {
+    console.log(`${e.message}`);
+    return new Response(`Failed to generate the image`, {
+      status: 500,
+    });
+  }
+}
--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['chrome-aws-lambda', 'playwright-core'],
+  },
   images: {
     dangerouslyAllowSVG: true,
     remotePatterns: [
