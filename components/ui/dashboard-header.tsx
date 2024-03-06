51a9d83f4327b2bff5399cf252b605297b49d614
cernockyd <24832873+cernockyd@users.noreply.github.com>
2024-03-06 23:09:51 +0100

    fix dashboard header position

--- components/ui/dashboard-header.tsx
+++ components/ui/dashboard-header.tsx
@@ -8,7 +8,7 @@ export default function DashboardHeader({
   className?: string;
 }) {
   return (
-    <div className="py-2 px-6 bg-background z-30 border-b border-neutral-200 sticky top-[53px] width-full">
+    <div className="py-2 px-6 bg-background z-30 border-b border-neutral-200 sticky top-[48px] width-full">
       <div className={classNames("flex", className)}>{children}</div>
     </div>
   );
